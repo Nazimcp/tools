117181751fea6817aacc663604b64701244ae47b
Nazimcp <75988681+Nazimcp@users.noreply.github.com>
2021-01-17 08:02:18 +0530

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+sleep 1
+figlet Cyber Black
+sleep 2
+echo "▇◤▔▔▔▔▔▔▔◥▇"
+echo "▇▏◥▇◣┊◢▇◤▕▇"
+echo "▇▏▃▆▅▎▅▆▃▕▇"
+echo "▇▏╱▔▕▎▔▔╲▕▇"
+echo "▇◣◣▃▅▎▅▃◢◢▇"
+echo "▇▇◣◥▅▅▅◤◢▇▇"
+echo "▇▇▇◣╲▇╱◢▇▇▇"
+echo "▇▇▇▇◣▇◢▇▇▇▇"
+echo "▇▇▇▇◣▇◢▇▇▇▇"
+echo "==WE ARE ANONYMOUS==";
+sleep 2
+echo "Author:Nazim Cp"
+echo "Email:nazimcp@gmail.com"
+echo "Telegram:@Cmastercode"
+echo "Github:Nazimcp"
+echo "Instagram:nazimcp7"
+echo "Youtube:Cyber_Black"
+echo "Facebook:NazimNazim"
+echo "Whatsapp:+91 7025604804"
+echo "Website:www.cyberblack.blogspot.com"
+echo "Snapchat:nazimcp";
+sleep 1
+echo "▄︻┻═┳一 🔴🔴--ЩєLc๏Mє ┼๏ ๏Цr ┼єrmЦx Pągє cyBєr_BLącK--🔴🔴"
+echo "▄︻┻═┳一 🔴--------♥️cyBєr_BLącK-๏FFIcIąL♥️---------🔴"
+echo "▄︻┻═┳一 🔴🔴----♥️♥️==cyBєr_BLącK==♥️♥️----🔴🔴"
+echo "▄︻┻═┳一 🔴🔴=====💪💪NAZIM CP💪💪=====🔴🔴"
+echo "▄︻┻═┳一 🔴🔴{========FєєL ┼ђє P๏Щєr========}🔴🔴"
+echo "▄︻┻═┳一 ЩєLc๏Mє ┼๏ cyBєr_BLącK"
+sleep 1
+echo "HEY GUYS CYBER BLACK IS HERE";
+sleep 1
+echo "THIS IS OUR INDRUDECTION PAGE IN TERMUX";
+sleep 1
+echo "WE WIL HELP YOU FOR ANY TECHNICAL ISSUE";
+sleep 1
+echo "YOU CAN ASK YOUR QUSTIONS ON INSTAGRAM [Time 06:00AM TO 06:00PM]";
+sleep 1
+echo "PLEASE SUBSCRIBE OUR CHANNEL";
+sleep 1
+echo "IN OUR CHANNEL WE WILL UPLAOD THE REAL TYPE OF HACKING AND SOME GAME HACKING TOTORIAL";
+sleep 1
+echo "IF YOU ARE INTRESTED IN WEB DEVELOPMENT,APP DEVELOPMENT,TERMUX TOOL DEVELOPMENT AND WEB DESINGING YOU CAN SAY TO ME IN THE INSTAGRAM OR ANY OTHER SOSCIAL MEDIA";
+sleep 1
+echo "I WILL MAKE THE TOTORIAL VIDEO ON IT AND UPLOAD IT IN OUR YOUTUBE CHANNEL";
+sleep 1
+echo "SO THANKS FOR VISITING MY OUR TERMUX PAGE BYE";
+done
